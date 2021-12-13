9dac98f7b4d9d115377340285475c010c721c6f4
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-12-13 19:03:01 +0900

    Update ssh.sh

--- .github/ssh.sh
+++ .github/ssh.sh
@@ -2,7 +2,7 @@
 
 expect -c "
 set timeout 5
-spawn scp -r root@$1:/opt/minecraft_server/logs ./	
+spawn scp -o StrictHostKeyChecking=no -r root@$1:/opt/minecraft_server/logs ./	
 expect \"Password:\"
 send \"$2\n\"
 interact
